e07b3ff12a4bc359ba27aad16bcbf67de5b5be18
flloisee <llego.fl06@gmail.com>
2024-11-19 21:59:11 +0800

    woo

---
+++ script.js
@@ -0,0 +1,24 @@
+function reveal1() {
+    let Lateralus = document.getElementById("LateralusIMG");
+    let LateralusVideo = document.getElementById("LateralusVid");
+    Lateralus.classList.toggle("hidden");
+    LateralusVideo.classList.toggle("hidden");
+    LateralusVideo.play();
+    console.log("Test");
+}
+function reveal2() {
+    let sidVerstappenImg = document.getElementById("sidVerstappenImg");
+    let sidVerstappenVideo = document.getElementById("sidVerstappenVideo");
+    sidVerstappenImg.classList.toggle("hidden");
+    sidVerstappenVideo.classList.toggle("hidden");
+    sidVerstappenVideo.play();
+    console.log("Test");
+}
+function reveal3() {
+    let PUPHymnImg = document.getElementById("PUPHymnImg");
+    let PUPHymnVid = document.getElementById("PUPHymnVid");
+    PUPHymnImg.classList.toggle("hidden");
+    PUPHymnVid.classList.toggle("hidden");
+    PUPHymnVid.play();
+    console.log("Test");
+}
